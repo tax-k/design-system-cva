e1f37fa6e6ea5d97de8bd68097092fc04f370f51
Tax <taxfree9090@gmail.com>
2023-08-23 17:34:42 +0900

    feat(prettier)

--- src/components/Input/Input.types.ts
+++ src/components/Input/Input.types.ts
@@ -1,7 +1,7 @@
 import { VariantProps, cva } from 'class-variance-authority';
 
 export const BaseInputVariants = cva(
-  'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 font-sourceCodePro',
+  'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 font-sourceCodePro text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
 );
 
 export interface InputProps
